e4d577266e17daa384b7c90daaefe6c6fa7a299f
Edigleyston Velez <129987669+Edigleyston@users.noreply.github.com>
2023-08-28 16:24:04 -0300

    Update main.js
    Adicionando uma animação ao scroll.

--- main.js
+++ main.js
@@ -85,6 +85,27 @@ function animaScroll() {
 
 window.addEventListener('scroll', animaScroll)
 
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('[data-anime="scroll"]');
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        if (isSectionVisible)
+          section.classList.add('ativo');
+        else
+          section.classList.remove('ativo');
+      })
+    }
+
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+  }
+}
 
 //Voltando para inicio da pagina
 function scrollHome() {
@@ -114,3 +135,4 @@ window.addEventListener('scroll', scrollHome)
 initAccordion();
 initTabMenu();
 initScrollSuave();
+initAnimacaoScroll();
